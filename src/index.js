9d96d1eb45f96381e9441edfadbcd44369a455fe
Ndichu-shee <ndichujoyce8@gmail.com>
2020-08-10 20:55:21 +0300

    my react app

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import App.css from ./app.css
+
+
+class Displayproduct extends React.Component {
+
+  render() {
+  
+     return <h2>Happy shopping</h2>
+}
+;
+
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
